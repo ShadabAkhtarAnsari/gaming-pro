58f7eac3d8e52c62a32265ec864a1a8227a8f0bb
malik-innovations <help.msakhtar@gmail.com>
2026-03-31 17:23:24 +0530

    Removed conflicting tailwind import

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./frontend/src/**/*.{js,ts,jsx,tsx}", // Ye line bahut zaroori hai
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
